704a277af7c919e7bc364b83e0c994d932d9786e
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-08 14:10:47 +0100

    add structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,30 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+
+//STRUMENTI DA UTILIZZARE
+
+//console log
+//if/else
+//prompt
+//alert
+//number
+
+//SVOLGIMENTO
+
+//Chiedere al passeggero Km che vuole percorrere
+
+const km = Number(prompt("Quanti Chilometri vuoi percorrere?"));
+console.log(km);
+
+//Chiedere al passeggero quanti anni ha
+
+const age = Number(prompt("Quanti anni hai?"));
+console.log(age);
+
+//Calcolare prezzo in base ai km
+
+//Applicare sconto 20% per minorenni
+
+//applicare sconto 40% per over 65
